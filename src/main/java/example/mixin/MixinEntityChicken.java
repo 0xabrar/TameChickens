d693c54550c5b6a06bfe8a2a7c99834cfe5660d5
Abrar Hussain <abrar.a.hussain@gmail.com>
2019-02-14 17:20:48 -0500

    Cancel chicken actions.

--- src/main/java/example/mixin/MixinEntityChicken.java
+++ src/main/java/example/mixin/MixinEntityChicken.java
@@ -1,12 +1,12 @@
 package example.mixin;
 
 import net.minecraft.entity.passive.EntityChicken;
-import net.minecraft.client.entity.EntityPlayerSP;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.util.SoundEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,9 +17,12 @@ public abstract class MixinEntityChicken {
     private static final Logger LOGGER = LogManager.getLogger();
 
     @Inject(method = "livingTick", at = @At("HEAD"), cancellable = true)
-    public void onLivingTick(CallbackInfo ci) {
-        EntityPlayerSP entityplayer = Minecraft.getInstance().player;
-        entityplayer.dropItem(false);
-        LOGGER.info("player tick ran with mixin");
+    public void onLivingTick(CallbackInfo callbackInfo) {
+        callbackInfo.cancel();
+    }
+
+    @Inject(method = "getAmbientSound", at = @At("HEAD"), cancellable = true)
+    public void onAmbientSound(CallbackInfoReturnable<SoundEvent> callbackInfo) {
+        callbackInfo.cancel();
     }
 }
